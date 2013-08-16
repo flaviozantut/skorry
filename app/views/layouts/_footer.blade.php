5114b003781918fb59a90a3d8d83a8c160fabc23
flaviozantut <flaviozantut@gmail.com>
2013-08-15 23:50:02 -0300

    add fork me btn

--- app/views/layouts/_footer.blade.php
+++ app/views/layouts/_footer.blade.php
@@ -1,7 +1,10 @@
 @section('footer')
      <div class="container">
         <footer>
-        <p>powerd by <a href="http://flavio.pw">flaviozantut</a></p>
+        <p>
+            powerd by <a href="http://flavio.pw">flaviozantut</a> &nbsp; &nbsp; — <a title="Fork me on GitHub" alt="Fork me on GitHub" href="https://github.com/flaviozantut/skorry"><i class="gn-icon icon-github"></i></a>
+        </p>
+
         </footer>
     </div>
 @show
